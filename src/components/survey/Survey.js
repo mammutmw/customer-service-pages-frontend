f84aa74305652222d80b4ba84ad1755f004af93b
leroj3 <77491867+leroj3@users.noreply.github.com>
2021-03-03 14:53:37 +0100

    added smiley survey (#12)

---
+++ src/components/survey/Survey.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+import Angry from "../../../public/assets/img/smiley_angry_gray.svg"
+import Annoyed from "../../../public/assets/img/smiley_annoyed_gray.svg"
+import Happy from "../../../public/assets/img/smiley_happy_gray.svg"
+import Neutral from "../../../public/assets/img/smiley_neutral_gray.svg"
+import VeryHappy from "../../../public/assets/img/smiley_Very_happy_gray.svg"
+
+
+//styles
+import "./Survey.scss";
+
+const Survey = () => {
+    return (
+        <Fragment>
+            <div className="container">
+                <div className="container__title">
+                    <h4>How was your experience today?</h4>
+                </div>
+                <div className="container__images">
+                    <img src={Angry} alt="Angry" className="container__images__angry" />
+                    <img src={Annoyed} alt="Annoyed" className="container__images__annoyed" />
+                    <img src={Neutral} alt="Neutral" className="container__images__neutral" />
+                    <img src={Happy} alt="Happy" className="container__images__happy" />
+                    <img src={VeryHappy} alt="Very Happy" className="container__images__very-happy" />
+                </div>
+            </div>
+        </Fragment>
+    )
+
+
+}
+
+export default Survey;
